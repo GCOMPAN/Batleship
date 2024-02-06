c6f073ea8677dcbc3dcc20f607b91111c18e134e
Gab <gabriel.compan@mines-ales.org>
2024-02-06 11:10:21 +0100

    update files

--- BattleShip.App/obj/Debug/net8.0/BattleShip.App.AssemblyInfo.cs
+++ BattleShip.App/obj/Debug/net8.0/BattleShip.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef969cabed4104deebbc66e44402dd3d590830e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59c2032495fb5a35fbbc28542ceada55dacddf8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShip.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
