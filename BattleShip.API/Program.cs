c8c84d86674323cac2d5ac6f3474228b91b89690
Gab <gabriel.compan@mines-ales.org>
2024-02-06 14:08:50 +0100

    Call GenerateBoatsPos

--- BattleShip.API/Program.cs
+++ BattleShip.API/Program.cs
@@ -1,13 +1,16 @@
+using BattleShip.API.Services;
+using Battleship.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Ajoutez les services au conteneur.
+// En savoir plus sur la configuration de Swagger/OpenAPI à https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurez le pipeline de requêtes HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +19,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapGet("/", () =>
+{   
+    var gridService = new GridService();
+    return gridService.GenerateBoatsPos();
 })
-.WithName("GetWeatherForecast")
+
+.WithName("BattleShip")
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
