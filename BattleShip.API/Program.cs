82c0421c7cd80ad67c10e451dfb24a0ba57e1d67
Gab <gabriel.compan@mines-ales.org>
2024-02-08 09:59:55 +0100

    update

--- BattleShip.API/Program.cs
+++ BattleShip.API/Program.cs
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", (GridService gridService) =>
+app.MapGet("/StartGameIA", (GridService gridService) =>
 {   
     return gridService.GenerateBoatsPos();
 })
