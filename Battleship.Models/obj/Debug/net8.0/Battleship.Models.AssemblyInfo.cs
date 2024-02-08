59982a3a7bc178d10003ffa3108937e55fb5b461
NicolasPois01 <pois.nicolas01@gmail.com>
2024-02-08 10:40:23 +0100

    .

--- Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
+++ Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff6a506fbb82a1454216c8f74cbbd5944d66e0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2b7c4677953ae7231a0b82be0a8c93ccffa5ce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
