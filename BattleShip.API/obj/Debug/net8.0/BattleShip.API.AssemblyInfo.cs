a55d251782c7a263403884745323e786ffb4a6a6
Gab <gabriel.compan@mines-ales.org>
2024-02-08 08:52:11 +0100

    Update Front du 07/02

--- BattleShip.API/obj/Debug/net8.0/BattleShip.API.AssemblyInfo.cs
+++ BattleShip.API/obj/Debug/net8.0/BattleShip.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32aefa2231d2d291b10f124f592cb6a31879de83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3485afd43e4640fd47b4396892f087f89a5e2fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
