7ff6a506fbb82a1454216c8f74cbbd5944d66e0f
NicolasPois01 <pois.nicolas01@gmail.com>
2024-02-08 09:20:19 +0100

    .

--- BattleShip.App/obj/Debug/net8.0/BattleShip.App.AssemblyInfo.cs
+++ BattleShip.App/obj/Debug/net8.0/BattleShip.App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3485afd43e4640fd47b4396892f087f89a5e2fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47102f622057e55cf4f1c0f3efca5503d857af5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShip.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
