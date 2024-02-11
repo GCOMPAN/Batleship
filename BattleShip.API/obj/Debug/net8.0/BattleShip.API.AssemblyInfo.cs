6255b03e78b41e727696cc64ffd266d5c1cf4317
Gab <gabriel.compan@mines-ales.org>
2024-02-11 13:35:04 +0100

    Red Grid when Hit

--- BattleShip.API/obj/Debug/net8.0/BattleShip.API.AssemblyInfo.cs
+++ BattleShip.API/obj/Debug/net8.0/BattleShip.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16daace554be6123d5da00be55fbf88315f3b891")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ead6ac505a526d6d14bf7269625abd23617ff49")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
