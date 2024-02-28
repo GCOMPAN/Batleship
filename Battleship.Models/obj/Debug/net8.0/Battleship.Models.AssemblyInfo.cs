b13b0d5e81b07aaee922219b60619fab249601be
Tom <tom.muet@orange.fr>
2024-02-28 17:32:13 +0100

    aaa

--- Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
+++ Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d803e1df49c21297211e781e76f9a027fc6243e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee66925e9e279dd6d1dcd60a797f42afede3f8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
