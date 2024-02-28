41c7e0b41e517b23b41f1f9d768a028959f6af8d
NicolasPois01 <pois.nicolas01@gmail.com>
2024-02-28 10:45:22 +0100

    Gameover handled

--- BattleShip.API/obj/Debug/net8.0/BattleShip.API.AssemblyInfo.cs
+++ BattleShip.API/obj/Debug/net8.0/BattleShip.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18aff4171c1879a138e384ad1c7ba31ebf9b6f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4543e13d35983c1235c8e8c6390785178bc7d969")]
 [assembly: System.Reflection.AssemblyProductAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BattleShip.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
