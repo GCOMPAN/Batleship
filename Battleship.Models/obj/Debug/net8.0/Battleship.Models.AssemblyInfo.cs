1c2468be373014fa69d4d24b4e472cea627bf5e8
Gab <gabriel.compan@mines-ales.org>
2024-02-29 11:56:08 +0100

    a

--- Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
+++ Battleship.Models/obj/Debug/net8.0/Battleship.Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef34bd7912f6ab9dd891fc3562d2648fdd315c98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d24313483d7bb9c900a074afa8068211c2b92edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
